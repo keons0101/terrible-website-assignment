948bbf38164b0d365c0550af767e0e0a8dccde06
Santiagoo <santiagominotta2913@hotmail.com>
2025-12-13 21:14:06 -0800

    Update script.js to hide scrollbar

--- script.js
+++ script.js
@@ -17,7 +17,22 @@ document.addEventListener('mousemove', (e) => {
     const centroY = rect.top + rect.height / 2;
     const distancia = Math.hypot(e.clientX - centroX, e.clientY - centroY);
     
-    if (distancia < 100) moverBtn();
+    if (distancia < 200) moverBtn();
+});
+
+btn.addEventListener('click', () => {
+
+    document.onmousemove = null;
+    
+    alert('YOU WON!');
+    
+    btn.style.background = '#4CAF50';
+    btn.style.color = 'white';
+    btn.style.fontWeight = 'bold';
+    btn.style.border = '3px solid #2E7D32';
+    btn.innerHTML = '¡GANADO! 🍌';
+    
+    btn.disabled = true;
 });
 
 window.onload = moverBtn;
